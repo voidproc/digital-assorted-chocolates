69f8b115e3abb0209d8e5e9e05a89b0ad7cdcaa9
voidProc <voidproc@gmail.com>
2023-02-10 01:36:53 +0900

    Initial commit

---
+++ digital-assorted-chocolates/Main.cpp
@@ -0,0 +1,53 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.6
+
+enum class Scenes
+{
+	Title,
+	Main,
+};
+
+void Main()
+{
+	constexpr Size sceneSize{ 128, 128 };
+	Window::Resize(sceneSize);
+
+	Scene::SetResizeMode(ResizeMode::Keep);
+
+	int scale = 4;
+	Window::Resize(sceneSize * scale);
+
+	Scene::SetTextureFilter(TextureFilter::Nearest);
+	const ScopedRenderStates2D renderState{ SamplerState::ClampNearest };
+
+	const Texture texture_title{ U"images/title.png" };
+
+
+	// Titleシーンから始まる
+
+	Scenes scene = Scenes::Title;
+
+
+	while (System::Update())
+	{
+		if (scene == Scenes::Title)
+		{
+			// Titleシーン
+
+			Scene::SetBackground(Palette::Hotpink);
+
+			texture_title.drawAt(Scene::CenterF());
+
+			if (MouseL.down())
+			{
+				scene = Scenes::Main;
+			}
+		}
+		else if (scene == Scenes::Main)
+		{
+			// Main シーン
+
+			Scene::SetBackground(Palette::Hotpink);
+
+		}
+	}
+}
